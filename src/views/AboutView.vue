cca42f9303cabd916eccdf5a5a48f70ba7fd90e6
NaoyaMiyagawa <tsuna2764@gmail.com>
2022-07-23 23:36:25 +0900

    build: add element-plus

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const selected = ref();
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="bg-light-200">
+      <ElButton>aaa</ElButton>
+
+      <div>
+        {{ selected }}
+        <ElSelect v-model="selected">
+          <ElOption disabled value="">Please select one</ElOption>
+          <ElOption value="1">1</ElOption>
+          <ElOption value="2">2</ElOption>
+          <ElOption value="3">3</ElOption>
+        </ElSelect>
+      </div>
+    </div>
   </div>
 </template>
 
