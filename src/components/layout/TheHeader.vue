b8e9358261626b4f7f40012bc9625b178ce474d7
NaoyaMiyagawa <tsuna2764@gmail.com>
2022-07-24 23:03:32 +0900

    feat: add header & element-plus icons

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+//
+const loginUser = $ref({ name: 'Tom' })
+</script>
+
+<template>
+  <ElHeader style="text-align: right; font-size: 12px">
+    <div class="toolbar">
+      <ElDropdown>
+        <ElIcon style="margin-right: 8px; margin-top: 1px"><setting /></ElIcon>
+
+        <template #dropdown>
+          <ElDropdownMenu>
+            <ElDropdownItem>View</ElDropdownItem>
+            <ElDropdownItem>Add</ElDropdownItem>
+            <ElDropdownItem>Delete</ElDropdownItem>
+          </ElDropdownMenu>
+        </template>
+      </ElDropdown>
+
+      <span>{{ loginUser.name }}</span>
+    </div>
+  </ElHeader>
+</template>
+
+<style lang="scss" scoped>
+.el-header {
+  position: relative;
+  background-color: var(--el-color-primary-light-7);
+  color: var(--el-text-color-primary);
+}
+
+.toolbar {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  right: 20px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,21 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './assets/main.scss'
 import 'uno.css'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
 app.use(createPinia())
 app.use(router)
 
