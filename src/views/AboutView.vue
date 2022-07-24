2a9c9980a84fbe78fac2c6b531709591b22ba970
NaoyaMiyagawa <tsuna2764@gmail.com>
2022-07-24 22:37:11 +0900

    build: add unocss

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const selected = $ref();
+const selected = $ref()
 </script>
 
 <template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,10 +10,11 @@ export default defineConfig({
     vue({
       reactivityTransform: true,
     }),
+    Unocss({}),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-});
+})
